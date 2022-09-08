3a14d9d5e0336d02b1b17264dbd38b6e24764fde
javier <jbollecich@gmail.com>
2022-09-08 12:08:17 -0300

    form basic in material

--- src/app/pages/form/form.module.ts
+++ src/app/pages/form/form.module.ts
@@ -6,8 +6,16 @@ import { UIModule } from '../../shared/ui/ui.module';
 import { FormRoutingModule } from './form-routing.module';
 
 import { LayoutsComponent } from './layouts/layouts.component';
+import {MatInputModule} from '@angular/material/input';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MatFormFieldModule, MAT_FORM_FIELD_DEFAULT_OPTIONS} from '@angular/material/form-field';
+import {MatSelectModule} from '@angular/material/select';
+
 
 @NgModule({
+  providers: [
+    {provide: MAT_FORM_FIELD_DEFAULT_OPTIONS, useValue: {appearance: 'fill'}}
+  ],
   // tslint:disable-next-line: max-line-length
   declarations: [LayoutsComponent],
   imports: [
@@ -15,6 +23,10 @@ import { LayoutsComponent } from './layouts/layouts.component';
     ReactiveFormsModule,
     FormRoutingModule,
     UIModule,
+    MatInputModule,
+    MatDatepickerModule,
+    MatFormFieldModule,
+    MatSelectModule
   
   ]
 })
--- src/app/pages/form/layouts/layouts.component.ts
+++ src/app/pages/form/layouts/layouts.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, Validators, FormGroup, FormBuilder } from '@angular/forms';
 
+
 @Component({
   selector: 'app-layouts',
   templateUrl: './layouts.component.html',
